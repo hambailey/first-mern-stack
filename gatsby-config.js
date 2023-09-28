42fd309e0723ecca91f432e1c44bf80010d00c78
hambailey <1325044+hambailey@users.noreply.github.com>
2023-09-27 20:06:53 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `First MERN Stack`,
+    siteUrl: `https://www.yourdomain.tld`
+  },
+  plugins: ["gatsby-plugin-netlify-cms"]
+};
