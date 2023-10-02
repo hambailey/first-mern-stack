8738914839c9843b6764af179877168b82a2a1de
hambailey <1325044+hambailey@users.noreply.github.com>
2023-10-02 03:43:56 -0400

    A quick update to blog pages.

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,14 +1,39 @@
 import * as React from 'react'
 import Layout from '../components/layout'
 import Seo from '../components/seo'
+import { graphql } from 'gatsby'
 
-const BlogPage = () => {
+const BlogPage = ({ data }) => {
     return (
+
         <Layout pageTitle="My Blog Post" >
-            <p>My cool posts will go in here.</p>
+            {
+                data.allMdx.nodes.map(node => (
+                    <article key={node.id}>
+                        <h2>{node.frontmatter.title}</h2>
+                        <p>Posted: {node.frontmatter.date}</p>
+                        <p>Abstract: {node.excerpt}</p>
+                    </article>
+                ))
+            }
         </Layout>
     )
 }
+
+export const frontMan = graphql`
+    query {
+        allMdx(sort: {frontmatter: {date: DESC}}) {
+            nodes {
+            frontmatter {
+                date(formatString: "MMMM D, YYYY")
+                title
+            }
+            id
+            excerpt
+            }
+        }
+    }
+`
 
 export const Head = () => < Seo title="My Blog Posts" />
 
