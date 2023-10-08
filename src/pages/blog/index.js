05ab350806a8dc461c09b3bb3af0ec1ff98f2b17
hambailey <1325044+hambailey@users.noreply.github.com>
2023-10-08 05:20:48 -0400

    Gatsby Tutorial 6 again. Trying to retrace back since mdx returns null. I think the problem is realizing Query Variables need to be written at the bottom of ___graphql.

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,7 +1,7 @@
 import * as React from 'react'
+import { Link, graphql } from 'gatsby'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
-import { Link, graphql } from 'gatsby'
 
 const BlogPage = ({ data }) => {
     return (
