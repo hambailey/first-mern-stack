b32946f19b23557ea81c3cfc1574c2042aa30af7
hambailey <1325044+hambailey@users.noreply.github.com>
2023-10-08 06:40:36 -0400

    Part7: added author credit to post body.

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -13,6 +13,12 @@ const BlogPost = ({ data, children }) => {
                 image={image}
                 alt={data.mdx.frontmatter.hero_image_alt}
             />
+            <p>
+                Photo Credit:{" "}
+                <a  href={data.mdx.frontmatter.hero_image_credit_link}>
+                    {data.mdx.frontmatter.hero_image_credit_text}
+                </a>
+            </p>
             {children}
         </Layout>
     )
