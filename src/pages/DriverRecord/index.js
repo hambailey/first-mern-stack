590a9c56099d982fb4be82a673d9440a56f66f1b
hambailey <1325044+hambailey@users.noreply.github.com>
2023-10-08 07:55:20 -0400

    DriverRecordIndex.js put inputs inside labels.

--- src/pages/DriverRecord/index.js
+++ src/pages/DriverRecord/index.js
@@ -25,20 +25,25 @@ const DriverRecordPage = () => {
             <p> Purpose: easy search list for truck drivers that have signed an orientation paper.</p>
             <p> Let's create a form. The form creates a seperate .mdx file per driver. (overkill on purpose to allow for more features later) </p>
             <form onSubmit={handleSubmit}>
-                <label htmlFor="name">Name:</label>
-                <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+                <label htmlFor="name">Name:
+                    <input type="text" id="name" name="name" value={formData.name} onChange={handleChange}/>
+                </label>
                 <br/>
-                <label htmlFor="carrier">Carrier:</label>
-                <input type="carrier" id="carrier" name="carrier" value={formData.carrier} onChange={handleChange}/>
+                <label htmlFor="carrier">Carrier:
+                    <input type="carrier" id="carrier" name="carrier" value={formData.carrier} onChange={handleChange}/>
+                </label>
                 <br/>
-                <label htmlFor="TWICExp">TWIC Expiration:</label>
-                <input type="text" id="TWICExp" name="TWICExp" value={formData.TWICExp} onChange={handleChange}/>
+                <label htmlFor="TWICExp">TWIC Expiration:
+                    <input type="text" id="TWICExp" name="TWICExp" value={formData.TWICExp} onChange={handleChange}/>
+                </label>
                 <br/>
-                <label htmlFor="product">Product:</label>
-                <input type="text" id="product" name="product" value={formData.product} onChange={handleChange}/>
+                <label htmlFor="product">Product:
+                    <input type="text" id="product" name="product" value={formData.product} onChange={handleChange}/>
+                </label>
                 <br/>
-                <label htmlFor="phoneNumber">Phone Number (optional):</label>
-                <input type="text" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
+                <label htmlFor="phoneNumber">Phone Number (optional):
+                    <input type="text" id="phoneNumber" name="phoneNumber" value={formData.phoneNumber} onChange={handleChange}/>
+                </label>
             </form>
         </Layout>
     )
