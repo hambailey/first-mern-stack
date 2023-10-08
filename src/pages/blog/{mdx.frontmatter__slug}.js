1fe3d4e61ca1883dae8c92fbdc9fe804abb44c20
hambailey <1325044+hambailey@users.noreply.github.com>
2023-10-08 06:26:09 -0400

    Part7 AddHeroImageUsingGatsbyImageComponent Step2&3: local webpage does not load.

--- src/pages/blog/{mdx.frontmatter__slug}.js
+++ src/pages/blog/{mdx.frontmatter__slug}.js
@@ -1,18 +1,24 @@
 import * as React from 'react'
 import {graphql} from 'gatsby'
+import { GatsbyImage, getImage } from 'gatsby-plugin-image'
 import Layout from '../../components/layout'
 import Seo from '../../components/seo'
 
 const BlogPost = ({ data, children }) => {
+    const image = getImage(data.mdx.frontmatter.hero_image)
     return (
         <Layout pageTitle={data.mdx.frontmatter.title} >
-            <p>{data.mdx.frontmatter.date}</p>
+            <p>Post Date: {data.mdx.frontmatter.date}</p>
+            <GatsbyImage
+                image={image}
+                alt={data.mdx.frontmatter.hero_image_alt}
+            />
             {children}
         </Layout>
     )
 }
 export const query = graphql`
-    query ($id: String) {
+    query($id: String) {
         mdx(id: {eq: $id}){
             frontmatter{
                 title
